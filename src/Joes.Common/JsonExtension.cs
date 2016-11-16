6707685816fa574d40bf4e94a359232a29c2ac9d
joes <joes@live.com>
2016-11-16 11:42:34 +0800

    添加项目文件。

---
+++ src/Joes.Common/JsonExtension.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Joes.Common
+{
+    public static class JsonExtension
+    {
+        public static string ToJson(this object obj, bool indented = false)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+
+            var setting = new JsonSerializerSettings();
+
+            setting.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            var format = Formatting.None;
+
+            if (indented) format = Formatting.Indented;
+
+            return JsonConvert.SerializeObject(obj, format, setting);
+        }
+
+        public static T JsonTo<T>(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) throw new ArgumentNullException(str);
+
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+
+        public static dynamic JsonTo(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) throw new ArgumentNullException(str);
+
+            JContainer container;
+
+            if (str.StartsWith("["))
+            {
+                container = JArray.Parse(str);
+            }
+            else
+            {
+                container = JObject.Parse(str);
+            }
+
+            dynamic result = container;
+
+            return result;
+        }
+
+        public static string JsonFormat(this string json)
+        {
+            var obj = JsonConvert.DeserializeObject(json);
+
+            return obj.ToJson(true);
+        }
+    }
+}
---
+++ src/Joes.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Joes.Common")]
+[assembly: AssemblyDescription("Common class library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Joes.Common")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
